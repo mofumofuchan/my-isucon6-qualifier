64ed7dc708fa1112ed1e38734ad4e84de5f44db0
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2019-04-27 13:25:32 +0900

    Add logging of isuda and isutar

--- config/unicorn/isuda.rb
+++ config/unicorn/isuda.rb
@@ -1,3 +1,6 @@
 worker_processes 5
 preload_app true
 timeout 120
+
+stderr_path "/home/isucon/webapp/ruby/log/isuda_stderr.log"
+stdout_path "/home/isucon/webapp/ruby/log/isuda_stdout.log"
--- config/unicorn/isutar.rb
+++ config/unicorn/isutar.rb
@@ -1,3 +1,6 @@
 worker_processes 5
 preload_app true
 timeout 120
+
+stderr_path "/home/isucon/webapp/ruby/log/isutar_stderr.log"
+stdout_path "/home/isucon/webapp/ruby/log/isutar_stdout.log"
